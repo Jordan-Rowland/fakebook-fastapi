fbd4a1fba0053af7b79f646113a380d838f5238a
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2023-04-08 08:50:51 -0700

    updates docker vars, req.txt

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,16 +3,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # sqlite
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./app/main.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./app/test.db"
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # mysql
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:rootpassword@localhost:3306/fakebook"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:rootpassword@localhost:3306/fakebook"
+# Change `localhost` to `host.docker.internal` for Docker
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:rootpassword@host.docker.internal:3306/fakebook"
 engine = sa.create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     try:
